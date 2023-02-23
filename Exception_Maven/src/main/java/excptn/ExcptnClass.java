31408942a1d6474298c75aab8afbe9305bddc1d2
reshm <reshm@LAPTOP-Q8U2HQJ0>
2023-02-23 11:00:03 +0530

    update

---
+++ Exception_Maven/src/main/java/excptn/ExcptnClass.java
@@ -0,0 +1,21 @@
+package excptn;
+
+public class ExcptnClass {
+	
+public static void main(String[] args) throws InterruptedException {
+		
+		
+		//throws keyword only for checked exception
+		//checkd exception we can handle by try catch or throws key word
+		//throws in method level/try catch statement level
+		
+		System.out.println("execution started");
+		Thread.sleep(3000);
+		
+		System.out.println("completed");
+		
+	
+
+	}
+
+}
